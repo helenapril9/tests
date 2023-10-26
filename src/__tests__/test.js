d525bab31190db000c06971610d1b0aafc58f020
Nelly Porvatova <dolgikhhel@yandex.ru>
2023-10-26 11:09:31 +0400

    Initial commit

---
+++ src/__tests__/test.js
@@ -0,0 +1,17 @@
+import  { checkHealth } from '../health.js';
+
+test('checkHealth', () => {
+    const person = {name: 'Маг', health: 90};
+    const result = checkHealth(person.health);
+    expect(result).toBe("healthy");
+})
+test('checkHealth', () => {
+    const person = {name: 'Маг', health: 14};
+    const result = checkHealth(person.health);
+    expect(result).toBe("critical");
+})
+test('checkHealth', () => {
+    const person = {name: 'Маг', health: 40};
+    const result = checkHealth(person.health);
+    expect(result).toBe("wounded");
+})
---
+++ src/health.js
@@ -0,0 +1,11 @@
+export function checkHealth(health) {    
+    let result = 0;
+    if (health < 15) {
+        result = "critical"};
+    if ( health >= 15 &&  health < 50) {
+       result = "wounded"};
+    if (health >= 50) {
+        result = "healthy"
+        };
+        return result;
+    };
