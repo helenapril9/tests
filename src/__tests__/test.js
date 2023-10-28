25a20171f964dfccaf8b443e1c78b09825a97f7b
Nelly Porvatova <dolgikhhel@yandex.ru>
2023-10-28 14:14:11 +0400

    add eslint

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,17 +1,17 @@
-import  { checkHealth } from '../health.js';
+import { checkHealth } from '../health';
 
 test('checkHealth', () => {
-    const person = {name: 'Маг', health: 90};
-    const result = checkHealth(person.health);
-    expect(result).toBe("healthy");
-})
+  const person = { name: 'Маг', health: 90 };
+  const result = checkHealth(person.health);
+  expect(result).toBe('healthy');
+});
 test('checkHealth', () => {
-    const person = {name: 'Маг', health: 14};
-    const result = checkHealth(person.health);
-    expect(result).toBe("critical");
-})
+  const person = { name: 'Маг', health: 14 };
+  const result = checkHealth(person.health);
+  expect(result).toBe('critical');
+});
 test('checkHealth', () => {
-    const person = {name: 'Маг', health: 40};
-    const result = checkHealth(person.health);
-    expect(result).toBe("wounded");
-})
+  const person = { name: 'Маг', health: 40 };
+  const result = checkHealth(person.health);
+  expect(result).toBe('wounded');
+});
